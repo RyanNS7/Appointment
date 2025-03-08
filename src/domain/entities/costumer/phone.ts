1695fb690c0ecdb7aa5ff87c2a647c9b27aff080
Ryanns7 <ryanns777@gmail.com>
2025-03-08 09:24:20 -0300

    feat: costumer phone entity

---
+++ src/domain/entities/costumer/phone.ts
@@ -0,0 +1,12 @@
+
+export class CostumerPhone{
+
+    phone: number
+    validated: Boolean
+
+    constructor(phone: number, validated: Boolean){
+        this.phone = phone
+        this.validated = validated
+    }
+
+}
