b98558c46d764240c85e778f07f21291991df458
Ryanns7 <ryanns777@gmail.com>
2025-03-12 16:38:09 -0300

    perf: adding the 'isValidDate' method to the repository

--- src/domain/usecases/AppointmentRepository.ts
+++ src/domain/usecases/AppointmentRepository.ts
@@ -1,4 +1,5 @@
 import { AppointmentCreationError } from "../../errors/appointmentCreationError";
+import { InvalidDateError } from "../../errors/invalidDateError";
 import { NotFoundError } from "../../errors/notFoundError";
 import { Appointment } from "../entities/appointment/appointment";
 import { AppointmentDTO } from "../entities/appointment/appointmentDTO";
@@ -7,4 +8,5 @@ export interface IAppointmentRepository{
     mark(appointment: Appointment): Promise<AppointmentDTO | AppointmentCreationError>
     unmark(appointmentID: string): Promise<Boolean | NotFoundError>
     getAppointment(appointmentID: string): Promise<AppointmentDTO | NotFoundError>
+    isValidDate(date: Date): Promise<Boolean | InvalidDateError>
 }
